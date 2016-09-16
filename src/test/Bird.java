881bd28d92597dba38347263ff60a25b14b159cf
Anastasios Fortis <ftasos2@gmail.com>
2016-09-17 02:10:09 +0300

    Allagh

---
+++ src/test/Bird.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test;
+
+/**
+ *
+ * @author tasos
+ */
+public class Bird {
+    
+    private int wings;
+
+    public Bird(int wings) {
+        this.wings = wings;
+    }
+    
+    
+    
+}
